5aa7fdc67970ad53ceb49cf58abc452101600410
Bryen Daco <72845402+BryenDaco@users.noreply.github.com>
2021-12-20 16:30:22 +0000

    Bug Fix

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const axios = require("axios");
 
+startToCheckScam();
+
+function startToCheckScam() {
+	checkScam();
+}
+
+function checkScam() {
+	module.exports = async (content) => {
+		try {
+			const loadAPI = await axios.get("https://bryendaco.github.io/catchscams.json");
+			if (!loadAPI) return startToCheckScam();
+
+			let response;
+
+			for (scams of loadAPI.data.scamlinks) {
+				if (!content) response = false;
+
+				if (content === scams || content.includes(scams)) {
+					response = true;
+					break;
+				} else response = false;
+
+			}
+			return response;
+		}
+	} catch (error) {
+		startToCheckScam();
+	}
+}
+
+/*
 module.exports = async (content) => {
 	const loadAPI = await axios.get("https://bryendaco.github.io/catchscams.json", { timeout: 1000 });
 	if (!loadAPI) throw Error("Could not load the API");
@@ -9,7 +40,7 @@ module.exports = async (content) => {
 	for (scams of loadAPI.data.scamlinks) {
 		if (!content) response = false;
 
-		if (content.includes(scams)) {
+		if (content === scams || content.includes(scams)) {
 			response = true;
 			break;
 		} else response = false;
@@ -17,3 +48,4 @@ module.exports = async (content) => {
 	}
 	return response;
 }
+*/
